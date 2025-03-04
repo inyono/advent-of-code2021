8a9bb95851c0e2febeccc2486055249d556323ab
Jonas Keinholz <git@inyono.dev>
2025-03-05 00:21:21 +0100

    Add black

--- tests/tests_day01.py
+++ tests/tests_day01.py
@@ -3,6 +3,7 @@ import unittest
 
 from src.day01 import Day01
 
+
 class Day01TestCase(unittest.TestCase):
     def setUp(self):
         example = """199
@@ -19,6 +20,7 @@ class Day01TestCase(unittest.TestCase):
 
     def test_something(self):
         self.assertEqual(7, self.day01.solve_part1())  # add assertion here
+
 
 if __name__ == "__main__":
     unittest.main()
